15bb4c7f03a36b49f3f861f95f8d72d4c37ed3a9
sweeppy <104524272+sweeppy@users.noreply.github.com>
2026-02-24 17:35:22 +0300

    Add logger for email verificaiton command

--- backend/Account/Account.Application/Features/Auth/Commands/EmailAuthentication/VerifyEmailCommand.cs
+++ backend/Account/Account.Application/Features/Auth/Commands/EmailAuthentication/VerifyEmailCommand.cs
@@ -1,10 +1,12 @@
 ﻿using Account.Application.Common.Errors;
 using Account.Application.Common.Results;
 using Account.Application.Features.Auth.Results;
+using Account.Application.Logger.Auth;
 using Account.Core.Repositories;
 using Account.Core.Repositories.Common;
 using Account.Core.Security;
 using MediatR;
+using Microsoft.Extensions.Logging;
 
 namespace Account.Application.Features.Auth.Commands.EmailAuthentication;
 
@@ -14,29 +16,45 @@ public record VerifyEmailCommand(string Email, string VerificationCode)
 public sealed class VerifyEmailCommandHandler(IUserRepository userRepository,
                                                IEmailVerificationCodeRepository codeRepository,
                                                IUnitOfWork unitOfWork,
-                                               IVerificationCodeHasher codeHasher)
+                                               IVerificationCodeHasher codeHasher,
+                                               ILogger<VerifyEmailCommandHandler> logger)
     : IRequestHandler<VerifyEmailCommand, ResultT<VerifyEmailResult>>
 {
     private readonly IUserRepository _userRepository = userRepository;
     private readonly IEmailVerificationCodeRepository _codeRepository = codeRepository;
     private readonly IUnitOfWork _unitOfWork = unitOfWork;
     private readonly IVerificationCodeHasher _codeHasher = codeHasher;
+    private readonly ILogger<VerifyEmailCommandHandler> _logger = logger;
 
     public async Task<ResultT<VerifyEmailResult>> Handle(VerifyEmailCommand request, CancellationToken token)
     {
+        _logger.LogStartVerifyEmail();
+
         var user = await _userRepository.GetByEmailAsync(request.Email, token);
         if (user is null)
+        {
+            _logger.LogUserWithSuchEmailNotFound(request.Email);
             return Error.NotFound(ErrorCodes.UserNotFound, ErrorMessages.UserNotFound);
+        }
 
         var codeEntity = await _codeRepository.GetNotExpiredByEmailAsync(request.Email, token);
         if (codeEntity is null)
+        {
+            _logger.LogVerificationCodeForThisEmailNotFound(request.Email);
             return Error.NotFound(ErrorCodes.VerificationCodeNotFound, ErrorMessages.VerificationCodeNotFound);
+        }
 
         if (codeEntity.IsUsed)
+        {
+            _logger.LogVerificationCodeAlreadyUsed(request.VerificationCode, request.Email);
             return Error.Failure(ErrorCodes.VerificationCodeAlreadyUsed, ErrorMessages.VerficaitonCodeAlreadyUsed);
+        }
 
         bool isCodeCorrect = _codeHasher.Verify(request.VerificationCode, codeEntity.HashedCode);
         if (!isCodeCorrect)
+        {
+            _logger.LogVerificationCodeForThisEmailNotCorrect(request.VerificationCode, request.Email);
+
             return Error.Validation(new Dictionary<string, string[]>
                 {
                     {
@@ -44,12 +62,15 @@ public sealed class VerifyEmailCommandHandler(IUserRepository userRepository,
                         new[] { ErrorMessages.InvalidVerificationCode }
                     }
                 });
+        }
 
         codeEntity.UseCode();
 
         await _unitOfWork.SaveChangesAsync(token);
 
         //TODO: return JWT token
+        
+        _logger.LogSuccessfulEmailVerification(request.VerificationCode, request.Email);
 
         return new VerifyEmailResult(IsCodeCorrect: true);
     }
---
+++ backend/Account/Account.Application/Logger/Auth/VerifyEmailCommandLogger.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Account.Application.Logger.Auth;
+
+public static partial class VerifyEmailCommandLogger
+{
+    [LoggerMessage(
+        EventId = 1100,
+        Level = LogLevel.Information,
+        Message = "Start verify email command")]
+    public static partial void LogStartVerifyEmail(this ILogger logger);
+
+    [LoggerMessage(
+        EventId = 1101,
+        Level = LogLevel.Warning,
+        Message = "User with email '{Email}' not found")]
+    public static partial void LogUserWithSuchEmailNotFound(this ILogger logger, string email);
+
+
+    [LoggerMessage(
+        EventId = 1102,
+        Level = LogLevel.Warning,
+        Message = "Verification code for email '{Email}' not found")]
+    public static partial void LogVerificationCodeForThisEmailNotFound(this ILogger logger, string email);
+
+    [LoggerMessage(
+        EventId = 1103,
+        Level = LogLevel.Warning,
+        Message = "Verification code '{Code}' for email '{Email}' has already used")]
+    public static partial void LogVerificationCodeAlreadyUsed(this ILogger logger, string code, string email);
+
+    [LoggerMessage(
+        EventId = 1104,
+        Level = LogLevel.Warning,
+        Message = "Verification code '{Code}' for email '{Email}' is not correct")]
+    public static partial void LogVerificationCodeForThisEmailNotCorrect(this ILogger logger, string code, string email);
+
+    [LoggerMessage(
+        EventId = 1105,
+        Level = LogLevel.Information,
+        Message = "Successful email verification; Email: '{Email}', Code: '{Code}'")]
+    public static partial void LogSuccessfulEmailVerification(this ILogger logger, string code, string email);
+}
